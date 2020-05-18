cbd44696cc936fd3a3574834bf8814e8a9bdb731
maerd19 <revolutions_333@hotmail.com>
2020-05-17 19:39:14 -0500

    polimorfismo

--- Java/Car.java
+++ Java/Car.java
@@ -1,8 +1,8 @@
 public class Car {
-    Integer id;
-    String license;
-    Account driver;
-    Integer passenger;
+    private Integer id;
+    private String license;
+    private Account driver;
+    private Integer passenger;
 
     public Car(String license, Account driver) {
         this.license = license;
@@ -10,6 +10,38 @@ public class Car {
     }
 
     void printDataCar() {
-        System.out.println("License: " + license + " Driver's name: " + driver.name);
+        if(passenger != null) {
+            System.out.println("License: " + license + " Driver's name: " + driver.name + " Passengers: " + passenger);
+        }        
     }
+
+    public Integer getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if(passenger == 4) {
+            this.passenger = passenger;
+        } else {
+            System.out.println("Necesitas asignar 4 pasajeros");
+        }
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    
 }
--- Java/Main.java
+++ Java/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        Car car = new Car("FAA123", new Account("Faustina Angeles", "INE"));
-        car.passenger = 4;
-        car.printDataCar();
+        Car uberX = new UberX("FAA123", new Account("Faustina Angeles", "INE"), "Chevrolter", "Sonic");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
 
-        Car car2 = new Car("RHA123", new Account("Rafael Hernandez", "INE"));
-        car2.passenger = 3;
-        car2.printDataCar();
+        UberVan uberVan = new UberVan("FJG998", new Account("Alejandra Torres", "ATA665"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+
+        // Car car2 = new Car("RHA123", new Account("Rafael Hernandez", "INE"));
+        // car2.passenger = 3;
+        // car2.printDataCar();
     }
 }
